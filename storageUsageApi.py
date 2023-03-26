e313f158ef1a9200f27bd16cca8a3e409ac88f6b
StevenSlewa <steven.slewa96@gmail.com>
2023-03-27 01:37:35 +0300

    first commit

--- storageUsageApi.py
+++ storageUsageApi.py
@@ -1,27 +1,28 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
 import firebase_admin
 import requests
 from firebase_admin import credentials, storage
 
-# Download the service account key file from the URL
-url = 'https://firebasestorage.googleapis.com/v0/b/raz-app-83361.appspot.com/o/_%2Fraz-app-83361-firebase-adminsdk-mtvdd-234e20e082.json?alt=media&token=72ff3535-5549-4bf8-8676-a0869e5269cc'
-response = requests.get(url)
-with open('serviceAccountKey.json', 'wb') as f:
-    f.write(response.content)
+app = Flask(__name__)
 
-# Initialize Firebase app with the downloaded credential
-cred = credentials.Certificate('serviceAccountKey.json')
-firebase_admin.initialize_app(cred, {'storageBucket': 'raz-app-83361.appspot.com'})
+@app.route('/')
+def storage_usage():
+    url = 'https://firebasestorage.googleapis.com/v0/b/raz-app-83361.appspot.com/o/_%2Fraz-app-83361-firebase-adminsdk-mtvdd-234e20e082.json?alt=media&token=72ff3535-5549-4bf8-8676-a0869e5269cc'
+    response = requests.get(url)
+    with open('serviceAccountKey.json', 'wb') as f:
+        f.write(response.content)
 
-# Get the storage client
-client = storage.bucket()
+    cred = credentials.Certificate('serviceAccountKey.json')
+    firebase_admin.initialize_app(cred, {'storageBucket': 'raz-app-83361.appspot.com'})
+    client = storage.bucket()
+    blobs = client.list_blobs()
+    current_usage = sum([blob.size for blob in blobs])
+    # total_allocation = 5 * 1024 * 1024 * 1024  # 5 GB in bytes
+    # remaining_storage = (total_allocation - current_usage) / 1024**3
+    print(current_usage)
+    return str(current_usage)
 
-blobs = client.list_blobs()
-current_usage = sum([blob.size for blob in blobs])
-
-# Get the total allocation based on the pricing plan
-# Replace the values in <pricing-plan> with your actual pricing plan
-total_allocation = 5 * 1024 * 1024 * 1024  # 5 GB in bytes
-
-remaining_storage = (total_allocation - current_usage) / 1024**3
-
-print(f'Remaining storage: {remaining_storage:.2f} GB')
+storage_usage()
