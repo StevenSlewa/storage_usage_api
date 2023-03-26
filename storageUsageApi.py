0dbde23a0ab99f72591a1a300e5dc0a5ab29a2ae
StevenSlewa <74624586+StevenSlewa@users.noreply.github.com>
2023-03-26 03:23:43 +0300

    Update storageUsageApi.py

--- storageUsageApi.py
+++ storageUsageApi.py
@@ -1,27 +1,39 @@
-import firebase_admin
+from flask import Flask, jsonify
 import requests
 from firebase_admin import credentials, storage
+import firebase_admin
 
-# Download the service account key file from the URL
-url = 'https://firebasestorage.googleapis.com/v0/b/raz-app-83361.appspot.com/o/_%2Fraz-app-83361-firebase-adminsdk-mtvdd-234e20e082.json?alt=media&token=72ff3535-5549-4bf8-8676-a0869e5269cc'
-response = requests.get(url)
-with open('serviceAccountKey.json', 'wb') as f:
-    f.write(response.content)
+app = Flask(__name__)
 
-# Initialize Firebase app with the downloaded credential
-cred = credentials.Certificate('serviceAccountKey.json')
-firebase_admin.initialize_app(cred, {'storageBucket': 'raz-app-83361.appspot.com'})
+@app.route('/storage-usage', methods=['GET'])
+def get_storage_usage():
+    # Download the service account key file from the URL
+    url = 'https://firebasestorage.googleapis.com/v0/b/raz-app-83361.appspot.com/o/_%2Fraz-app-83361-firebase-adminsdk-mtvdd-234e20e082.json?alt=media&token=72ff3535-5549-4bf8-8676-a0869e5269cc'
+    response = requests.get(url)
+    with open('serviceAccountKey.json', 'wb') as f:
+        f.write(response.content)
 
-# Get the storage client
-client = storage.bucket()
+    # Initialize Firebase app with the downloaded credential
+    cred = credentials.Certificate('serviceAccountKey.json')
+    firebase_admin.initialize_app(cred, {'storageBucket': 'raz-app-83361.appspot.com'})
 
-blobs = client.list_blobs()
-current_usage = sum([blob.size for blob in blobs])
+    # Get the storage client
+    client = storage.bucket()
 
-# Get the total allocation based on the pricing plan
-# Replace the values in <pricing-plan> with your actual pricing plan
-total_allocation = 5 * 1024 * 1024 * 1024  # 5 GB in bytes
+    blobs = client.list_blobs()
+    current_usage = sum([blob.size for blob in blobs])
 
-remaining_storage = (total_allocation - current_usage) / 1024**3
+    # Get the total allocation based on the pricing plan
+    # Replace the values in <pricing-plan> with your actual pricing plan
+    total_allocation = 5 * 1024 * 1024 * 1024  # 5 GB in bytes
 
-print(f'Remaining storage: {remaining_storage:.2f} GB')
+    remaining_storage = (total_allocation - current_usage) / 1024**3
+
+    response_data = {
+        'remaining_storage': remaining_storage
+    }
+
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
